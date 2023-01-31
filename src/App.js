3a0774098933cdc9ee3e4af7ca3277e0b4a17807
sounder717 <sounder717@gmail.com>
2023-01-31 10:22:22 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/App.css
    	deleted:    src/App.css.map
    	modified:   src/App.js
    	deleted:    src/App.scss
    	modified:   src/components/Allmatches/Allmatches.css
    	modified:   src/components/Allmatches/Allmatches.css.map
    	modified:   src/components/Allmatches/Allmatches.scss
    
    Changes not staged for commit:
    	modified:   src/components/Allmatches/Allmatches.css
    	modified:   src/components/Allmatches/Allmatches.css.map
    	modified:   src/components/Allmatches/Allmatches.scss

--- src/App.js
+++ src/App.js
@@ -3,19 +3,59 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { useState } from "react";
 import Allmatches from "./components/Allmatches/Allmatches";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 function App() {
   const [data, setdata] = useState([
-    { Option: "Cric news" },
-    { Option: "IPL" },
-    { Option: "Ind vs NZ" },
-    { Option: "BBL" },
-    { Option: "Match prediction" },
+    { Option: "MATCH1" },
+    { Option: "MATCH2" },
+    { Option: "MATCH3" },
   ]);
   return (
     <div className="App" style={{ overflow: "hidden" }}>
       <Router>
-        <nav>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand as={Link} to="/">
+              <img src={logo} className="logo_img"></img>
+              Sports Voice Tamil
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link as={Link} to="/">
+                  ALL MATCHES
+                </Nav.Link>
+                <Nav.Link as={Link} to="/">
+                  IPL
+                </Nav.Link>
+                <NavDropdown
+                  title="INTERNATIONAL MATCHES"
+                  id="collasible-nav-dropdown"
+                >
+                  {data.map((e, i) => (
+                    <NavDropdown.Item as={Link} to="/">
+                      {e.Option}
+                    </NavDropdown.Item>
+                  ))}
+                </NavDropdown>
+              </Nav>
+              <Nav>
+                <Nav.Link as={Link} to="/">
+                  OTHER MATCHES
+                </Nav.Link>
+                <Nav.Link eventKey={2} as={Link} to="/">
+                  MATCH PREDICTION
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        {/* <nav>
           <div className="brand">
             <Link to="/">
               <img src={logo} className="logo_img"></img>
@@ -37,7 +77,7 @@ function App() {
               </li>
             ))}
           </ul>
-        </div>
+        </div> */}
         <Routes>
           <Route path="/" element={<Allmatches />}></Route>
         </Routes>
